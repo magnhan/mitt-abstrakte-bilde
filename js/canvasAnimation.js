ccf27a599ec0d6b56a574b038ed87b6f3d173b02
magnhan <45832668+magnhan@users.noreply.github.com>
2020-08-28 20:12:51 +0200

    Progress on animation

--- js/canvasAnimation.js
+++ js/canvasAnimation.js
@@ -4,9 +4,13 @@ var ctx = c.getContext("2d");
 var summerSkyGrad = ctx.createLinearGradient(0, 0, 0, 300);
 summerSkyGrad.addColorStop(0, "lightblue");
 summerSkyGrad.addColorStop(1, "#4498ff");
+var winterSkyGrad = ctx.createLinearGradient(0, 0, 0, 300);
+winterSkyGrad.addColorStop(0, "rgba(255,255,255,0)");
+winterSkyGrad.addColorStop(1, "rgba(255,255,255,1)");
 var grassGrad = ctx.createLinearGradient(0, 250, 0, 300);
 grassGrad.addColorStop(0, "#6fc960");
 grassGrad.addColorStop(1, "#58a54b");
+
 var mountainGrad = ctx.createLinearGradient(0, 250, 0, 120);
 mountainGrad.addColorStop(0, "#606060");
 mountainGrad.addColorStop(1, "#a0a0a0");
@@ -14,11 +18,32 @@ var sunGrad = ctx.createRadialGradient(250,150,0,250,150,100)
 sunGrad.addColorStop(0, "rgba(211,164,0,1)");
 sunGrad.addColorStop(1, "rgba(255,255,255,0)");
 
-drawSummerSky();
-drawSun();
-drawGrass();
-drawMountains();
+const artCanvas = document.getElementById("artCanvas");
 
+drawSummer();
+
+artCanvas.addEventListener("mouseenter", e => {
+    drawWinter();
+});
+
+artCanvas.addEventListener("mouseleave", e => {
+    drawSummer();
+});
+
+function drawSummer(alpha){
+    drawSummerSky();
+    drawSun();
+    drawGrass();
+    drawMountains();
+    drawBird();
+};
+
+function drawWinter(){
+    drawSummerSky();
+    drawWinterSky();
+    drawSnow(0, 0.1);
+    drawMountains();
+}
 
 function drawSun(){
     ctx.beginPath();
@@ -43,12 +68,66 @@ function drawMountains(){
     ctx.fill(); //fjell
 };
 
+function drawBird(){
+    ctx.fillStyle= "#424242";
+    ctx.beginPath();
+    ctx.moveTo(265,150);
+    ctx.lineTo(275,140);
+    ctx.lineTo(268,155);
+    ctx.lineTo(272,157);
+    ctx.lineTo(268,156);
+    ctx.lineTo(257,153);
+    ctx.lineTo(259,151);
+    ctx.lineTo(255,145);
+    ctx.closePath();
+    ctx.fill();
+}
+
 function drawGrass(){
     ctx.fillStyle = grassGrad;
     ctx.fillRect(0,250,400,50); // gress
 };
 
+function drawSnow(a, d){
+    alpha = a;
+    delta = d;
+    while(a <= 1){
+        var snowGrad = ctx.createLinearGradient(0, 250, 0, 300);
+        snowGrad.addColorStop(0, `rgba(243,243,243,${a}`);
+        snowGrad.addColorStop(1, "rgba(214,214,214,1");
+        ctx.fillStyle = snowGrad;
+        ctx.fillRect(0,250,400,50);
+        console.log("%a", a);
+        a+=d;
+    }
+}
+
 function drawSummerSky(){
     ctx.fillStyle = summerSkyGrad; 
     ctx.fillRect(0,0,400,300); //sommerhimmel
 };
+
+function drawWinterSky(){
+    ctx.fillStyle = winterSkyGrad; 
+    ctx.fillRect(0,0,400,300); //vinterhimmel
+};
+
+var alpha = 0,   /// current alpha value
+    delta = 0.1; /// delta = speed
+
+
+function loop(){
+    alpha += delta;
+    if(alpha >= 1) stop();
+
+}
+/*function loop() {
+    alpha += delta;
+    if (alpha => 1) stop();
+    /// clear canvas, set alpha and re-draw image
+    ctx.clearRect(0, 0, demo.width, demo.height);
+    ctx.globalAlpha = alpha;
+    ctx.drawImage(img, 0, 0);
+
+    requestAnimationFrame(loop); // or use setTimeout(loop, 16) in older browsers
+}*/
