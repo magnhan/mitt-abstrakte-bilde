4d28044122a78d2eed4bb85e8e4ef207b27b001e
magnhan <45832668+magnhan@users.noreply.github.com>
2020-08-28 09:50:32 +0200

    Added canvas and svg animation

--- js/jQueryAnimation.js
+++ js/jQueryAnimation.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
     $("#winterSky").hide();
+    $("#snow").hide();
     $("svg").mouseenter(function(){
         $("#svgBird").fadeOut();
         $("#winterSky").fadeIn();
         $("#sun").fadeOut();
+        $("#snow").fadeIn();
+        //$(".snowflake").animate({down: "400px"});
     });
     $("svg").mouseleave(function(){
         $("#svgBird").fadeIn();
         $("#winterSky").fadeOut();
         $("#sun").fadeIn();
+        $("#snow").fadeOut();
     });
 })
